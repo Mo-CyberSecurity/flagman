cb48e9431f832171d9929d886f0128eb09a04920
Mo-CyberSecurity <mo.cybersecurity@yandex.ru>
2022-10-19 23:35:35 +0300

    patch2

--- public/js/my.js
+++ public/js/my.js
@@ -1,59 +1,86 @@
 function checkLocalStorage() {
   for (let i = 0; i < 12; i++) {
     if (window.localStorage.getItem(`flag-${i}`) !== document.getElementsByClassName(`flag-${i}`)) {
-      document.getElementsByClassName(`flag-${i}`)[0].innerHTML = window.localStorage.getItem(`flag-${i}`)
+      document.getElementsByClassName(`flag-${i}`)[0].innerHTML = window.localStorage.getItem(
+        `flag-${i}`
+      );
     }
   }
 }
 
+function xssInjection() {
+  let name = document.getElementById("name").value;
+  fetch(`/v2?name=${name}`)
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+      document.getElementsByClassName("otvet")[0].innerHTML = data;
+    });
+}
+
 function getFlag() {
-  let fl = 'na derzi'
-  return fl
+  fetch("/hint?flag=1")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
 
 function changeInputBtnName() {
   var topPiraterWords = [
-    'Эй, на палубе!',
-    'Встать на якорь!',
-    'Тишина на палубе!',
-    'Смирно!',
-    'Рад увидеть твою рожу снова!',
-    'Освободите мои плавники, я хочу съесть еще!',
-    'Есть только один верный флаг, и он черный',
-    'Заткнись и дай пройти!',
-    'Катитесь к Дейви Джонсу',
-    'Он будет болтаться, как клещи на ветру!',
-    'Он отдал концы',
-    'Якорь мне в бухту!',
-    'Он принял слишком много рома на борт.',
-    'Коробка вонючих костей',
-    'Трусливый щенок!',
-    'Портовая крыса!',
-    'Кальмарьи кишки!',
-    'Попутного ветра!',
-    'Спокойного моря!',
-    'Полных парусов и сухого пороха!',
-    'flag',
-    'flag']
-  document.getElementsByClassName('pirate-btn')[0].textContent = topPiraterWords[Math.floor(Math.random() * topPiraterWords.length)]
+    "Эй, на палубе!",
+    "Встать на якорь!",
+    "Тишина на палубе!",
+    "Смирно!",
+    "Рад увидеть твою рожу снова!",
+    "Освободите мои плавники, я хочу съесть еще!",
+    "Есть только один верный флаг, и он черный",
+    "Заткнись и дай пройти!",
+    "Катитесь к Дейви Джонсу",
+    "Он будет болтаться, как клещи на ветру!",
+    "Он отдал концы",
+    "Якорь мне в бухту!",
+    "Он принял слишком много рома на борт.",
+    "Коробка вонючих костей",
+    "Трусливый щенок!",
+    "Портовая крыса!",
+    "Кальмарьи кишки!",
+    "Попутного ветра!",
+    "Спокойного моря!",
+    "Полных парусов и сухого пороха!",
+    "FLAG",
+  ];
+  document.getElementsByClassName("pirate-btn")[0].textContent =
+    topPiraterWords[Math.floor(Math.random() * topPiraterWords.length)];
 }
 
 function requestAPI() {
-  fetch('/api?flag=' + document.getElementsByClassName('input')[0].value)
+  fetch("/api?flag=" + document.getElementsByClassName("input")[0].value)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      if (data.status === 'poydet') {
-        window.localStorage.setItem(`flag-${data.flag_number}`, data.flag)
+      if (data.status === "Молодчинка😘") {
+        window.localStorage.setItem(`flag-${data.flag_number}`, data.flag);
       }
 
-      document.getElementsByClassName('message')[0].innerHTML = 'Ответ: ' + data.status
-      checkLocalStorage()
-      changeInputBtnName()
+      document.getElementsByClassName("message")[0].innerHTML = "Ответ: " + data.status;
+      checkLocalStorage();
+      changeInputBtnName();
     });
 }
 
-if (window.localStorage.getItem(`flag-0`) === 'flag') {
-  checkLocalStorage()
+if (window.localStorage.getItem(`flag-0`) === "flag") {
+  checkLocalStorage();
+}
+
+if (!window.localStorage.length) {
+  for (let i = 0; i < 12; i++) {
+    window.localStorage.setItem(`flag-${i}`, "*");
+  }
+  window.localStorage.setItem("Не менять", "Сломает страницу");
 }
--- server.js
+++ server.js
@@ -8,22 +8,51 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/" + "index.html");
 });
 
+app.get("*/hint*", (req, res) => {
+  req.query.flag === "1" ? res.send("derzi") : null;
+});
+
 app.get("/api*", (req, res) => {
-  var predictFlag = req.query.flag
+  var predictFlag = req.query.flag;
 
   switch (predictFlag) {
-    case 'ABOBUS123':
-      res.send({ status: 'Попробуй написать флаг еще раз, но уже головой, мб стоит просто много попробовать?)' })
+    case "ABOBUS123":
+      res.send({
+        status:
+          "Попробуй написать флаг еще раз, но уже головой, мб стоит просто много попробовать?)",
+      });
       break;
-    case 'FLAG':
-      res.send({ flag_number: 0, flag: 'flag', status: 'poydet' })
+    case "flag":
+      res.send({ status: "Регистр тоже важен)))" });
       break;
-    case 'na derzi':
-      res.send({ flag_number: 1, flag: 'ya@', status: 'poydet' })
+    case "FLAG":
+      res.send({ flag_number: 0, flag: "flag", status: "Молодчинка😘" });
       break;
+    case "derzi":
+      res.send({ flag_number: 1, flag: "ya@", status: "Молодчинка😘" });
+      break;
+    case "_1B*C3":
+      res.send({ flag_number: 2, flag: "ti@", status: "Молодчинка😘" });
+      break;
+    case "HELLOWORLD":
+      res.send({ flag_number: 7, flag: "k3!", status: "Молодчинка😘" });
     default:
-      res.send({ status: 'try again' })
+      res.send({ status: "Попробуй еще солнце❤️" });
       break;
+  }
+});
+
+app.get("/v2*", (req, res) => {
+  const flag = {
+    name: "Alex",
+    email: "alex@site.com",
+    role: "user",
+    flag: "helloWorld",
+  };
+  if (req.query.name.indexOf("<script>") !== -1 && req.query.name.indexOf("</script>") !== -1) {
+    res.send(eval(req.query.name.substr(8, req.query.name.length - 17)));
+  } else {
+    res.send(req.query.name);
   }
 });
 
